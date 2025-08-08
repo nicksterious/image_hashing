919b9f49060baa75d2f033c0a36fbd8fdffecb00
CheckThisOut <86164655+nicksterious@users.noreply.github.com>
2025-08-08 16:57:08 +0300

    bump version

--- lib/image_hashing.rb
+++ lib/image_hashing.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 require_relative "image_hashing/version"
-require "image_processing/vips"
 require "vips"
+require "image_processing/vips"
 require "json"
 require "digest"
 require "dhash-vips"
