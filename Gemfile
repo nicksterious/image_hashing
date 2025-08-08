d6ad701f917887ee0c11761f27a34ca87d21e8a1
CheckThisOut <86164655+nicksterious@users.noreply.github.com>
2025-08-08 16:14:06 +0300

    initial commit

---
+++ Gemfile
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+source "https://rubygems.org"
+
+# Specify your gem's dependencies in image_hashing.gemspec
+gemspec
+
+gem "rake", "~> 13.0"
+
+gem "rspec", "~> 3.0"
+
+
+gem "ruby-vips", "~> 2.2.4"
+gem "image_processing", "~> 1.14.0"
+gem "vips"
+gem "digest"
+gem "dhash-vips", "~> 0.2.4.0"
---
+++ lib/image_hashing.rb
@@ -0,0 +1,82 @@
+# frozen_string_literal: true
+
+require_relative "image_hashing/version"
+require "image_processing/vips"
+require "vips"
+require "json"
+require "digest"
+require "dhash-vips"
+
+module ImageHashing
+  class Error < StandardError; end
+
+  class ImageHash
+    def initialize(file_name)
+      @image = Vips::Image.new_from_file(file_name)
+      @output = {
+          file_name: file_name,
+          file_size_bytes: File.size(file_name),
+          file_md5_hash: Digest::MD5.file(file_name).hexdigest,
+          width: @image.width,
+          height: @image.height,
+          format: @image.format,
+          original: {
+              dhash: DHashVips::DHash.calculate(@image).to_s,
+              idhash: DHashVips::IDHash.fingerprint(@image).to_s,
+              histogram: get_histogram(@image)
+          }
+      }
+    end
+    
+
+    def generate
+      # get some metadata based on several other resolutions
+      image = ImageProcessing::Vips.source(@image)
+      image = image.convert("png")
+      [800, 400, 200, 100, 50].each do |size|
+          resized_image = image.resize_to_limit(size, size).call(save: false)
+          @output["#{size}x#{size}"] = {
+              size: size,
+              dhash: DHashVips::DHash.calculate(resized_image).to_s,
+              idhash: DHashVips::IDHash.fingerprint(resized_image).to_s,
+              histogram: get_histogram(resized_image)
+          }
+      end
+      @output
+    end # hash
+
+
+    def get_rgb_qties
+        image = @image
+        if image.format.to_s != "uchar"  
+            image = image.colourspace(:rgb)  # Convert to RGB if necessary
+        end
+
+        # Split the image into R, G, and B channels
+        red, green, blue = image.bandsplit
+
+        # Calculate histograms for each channel
+        red_hist = red.hist_find_indexed(0)
+        green_hist = green.hist_find_indexed(0)
+        blue_hist = blue.hist_find_indexed(0)
+
+        # Total number of pixels for each color channel is the sum of the histogram
+        {
+            red: red_hist.to_a.first.first.first,
+            green: green_hist.to_a.first.first.first,
+            blue: blue_hist.to_a.first.first.first
+        }  
+    end # get_rgb_qties
+
+    def get_histogram
+        image = @image
+        begin
+            return get_rgb_qties(image)
+        rescue
+           nil
+        end
+        return { red: 0, green: 0, blue: 0 }
+    end # get_histogram
+
+  end # class
+end
